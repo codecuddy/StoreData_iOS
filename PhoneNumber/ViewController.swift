b3a1969b41b0219593a3c8512dea960c42036fec
Charlie <charliecuddy4@gmail.com>
2017-11-29 23:36:42 -0600

    Save a phone number when button pressed

--- PhoneNumber/ViewController.swift
+++ PhoneNumber/ViewController.swift
@@ -10,9 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var phoneNumber: UITextField!
+    
+    @IBOutlet weak var savedNumber: UILabel!
+    
+    @IBAction func submitNumb(_ sender: Any) {
+        UserDefaults.standard.set(phoneNumber.text, forKey: "number")
+        savedNumber.text = "Number Submitted"
+        print("Number Submitted")
+        viewDidLoad()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("page loaded")
         // Do any additional setup after loading the view, typically from a nib.
+    
+        let numberObject = UserDefaults.standard.object(forKey: "number")
+    
+        if let newNumber = numberObject as? String {
+            //savedNumber.text = "Your submitted number is: \n\(newNumber)"
+            print("The submitted number is: \(newNumber)")
+            phoneNumber.text = newNumber
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
